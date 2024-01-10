86bf7734fe4d1843f8c07cccf464eeed69f8e026
Ross Watts <ross.n.watts@gmail.com>
2024-01-10 16:25:00 +0000

    Updated project name in build.yml

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,7 +4,6 @@ public static class ModHelperData
 {
     public const string Version = "1.0.0";
     public const string Name = "Bloonarius Mastery Mode";
-
     public const string Description = "Upgrades bloon ranks, replacing BADs and Fortified BADs with 3x Speed Tier 3 Bloonarius and Elite Bloonarius. Also starts reducing cash per pop 10 rounds early.";
     public const string WorksOnVersion = "34";
     public const string RepoOwner = "Ross-TheBoss";
